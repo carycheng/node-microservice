8dcde22ac16d64a5d69a596b0348476578d70853
carycheng <carycheng77@gmail.com>
2022-02-01 20:25:51 -0800

    adding payments service

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,8 +4,11 @@ import {
     requireAuth,
     validateRequest,
     BadRequestError,
-    NotFoundError
+    NotFoundError,
+    NotAuthorizedError,
+    OrderStatus,
 } from '@msticketsdev/common';
+import { stripe } from '../stripe';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -22,7 +25,27 @@ router.post('/api/payments',
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        res.send({ success: true })
+        const { token, orderId } = req.body;
+
+        const order = await Order.findById(orderId);
+
+        if (!order) {
+            throw new NotFoundError();
+        }
+        if (order.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
+        if (order.status === OrderStatus.Cancelled) {
+            throw new BadRequestError('Cannot pay for a cancelled order');
+        }
+
+        await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token,
+        });
+
+        res.send({ success: true });
 });
 
 export { router as createChargeRouter };
